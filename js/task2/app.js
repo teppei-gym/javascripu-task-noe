9ff0b5b092a4490f92da23b3b5fef73004a63a3b
teppei <yamamutaisou@gmail.com>
2020-09-13 19:32:47 +0900

    課題2修正

--- js/task2/app.js
+++ js/task2/app.js
@@ -1,59 +1,52 @@
+'use strict';
+
 const fizz = document.getElementById('fizz');
 const buzz = document.getElementById('buzz');
 const runBtn = document.getElementById('run-btn');
 const wrap = document.getElementById('wrap');
 
 runBtn.addEventListener('click', function () {
-  while ((el = wrap.firstChild)) {
-    wrap.removeChild(el);
-  }
+  wrap.innerHTML = "";
 
-  const initFizz = Number(fizz.value);
-  const initBuzz = Number(buzz.value);
+  const initFizz = fizz.value;
+  const initBuzz = buzz.value;
 
-  if (typeof initFizz !== 'number' || typeof initBuzz !== 'number') {
-    console.error('数字を入力してください');
+  if (!isNumber(initFizz) || !isNumber(initBuzz)) {
+    const el = document.createElement('p');
+    el.textContent = '整数を入力してください';
+    el.style.color = 'red';
+    wrap.appendChild(el);
+
     return;
   }
 
-  let result = [];
-
-  let fizzNum = 0;
-  for (let i = 0; fizzNum < 100; i++) {
-    fizzNum = initFizz * i;
-    if (fizzNum < 100) result.push(fizzNum);
-  }
-
-  let buzzNum = 0;
-  for (let i = 0; buzzNum < 100; i++) {
-    buzzNum = initBuzz * i;
-    if (buzzNum < 100) result.push(buzzNum);
-  }
-
-  result.sort((a, b) => {
-    return a - b;
-  })
-
-  multiple = new Set(result);
-  multiple.delete(0);
-
-  for (num of multiple) {
-    output(num, initFizz, initBuzz);
+  for (let i = 1; i < 100; i++) {
+    output(i, initFizz, initBuzz);
   }
 });
 
 function output(num, initFizz, initBuzz) {
-  prefix = '';
+  let prefix = '';
 
   if (num % initFizz === 0 && num % initBuzz === 0) {
     prefix = 'FizzBuzz';
   } else if (num % initFizz === 0) {
     prefix = 'Fizz';
-  } else {
+  } else if (num % initBuzz === 0) {
     prefix = "Buzz";
+  } else {
+    return
   }
 
   const el = document.createElement('div');
   el.textContent = `${prefix} ${num}`;
   wrap.appendChild(el);
+}
+
+function isNumber(arg) {
+  if (arg && Number.isInteger(Number(arg))) {
+    return true;
+  } else {
+    return false;
+  }
 }
