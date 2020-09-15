7c734e64741bcc0f03574e0aaad2a44aeebb7ee3
teppei <yamamutaisou@gmail.com>
2020-09-15 21:44:33 +0900

    課題３−２完了

--- js/task3/app.js
+++ js/task3/app.js
@@ -1,31 +1,43 @@
 const addBtn = document.getElementById('add-task-btn');
 let maxTaskId = 0;
 const table = document.getElementById('task-table');
+const deleteBtn = document.getElementById('delete-btn')
+const task = document.getElementById('task-text');
 
 addBtn.addEventListener('click', function () {
-  const task = document.getElementById('task-text');
+  const taskVal = task.value;
+  if (taskVal === "") return;
 
-  output(task.value, table);
+  output(taskVal, table);
   task.value = "";
 });
 
 function output(task, table) {
   const tr = document.createElement('tr');
-  const id = document.createElement('td');
-  const td = document.createElement('td');
 
-  const appendTr = (tagName, value) => {
+  const appendTr = (tagName, value, classNanme = null) => {
     const el = document.createElement(tagName);
+    if (classNanme !== null) {
+      el.classList.add(classNanme);
+    }
+    if (value === '削除') {
+      el.addEventListener('click', function (e) {
+        e.target.parentNode.remove();
+        const idElements = document.querySelectorAll('.task-id');
+        maxTaskId = 0;
+        for (let el of [...idElements]) {
+          el.textContent = ++maxTaskId;
+        }
+      })
+    }
     el.textContent = value;
     tr.appendChild(el);
   };
 
-  appendTr('td', ++maxTaskId);
+  appendTr('td', ++maxTaskId, 'task-id');
   appendTr('td', task);
   appendTr('button', '作業中');
   appendTr('button', '削除');
 
-
-  tr.appendChild(td);
   table.lastElementChild.appendChild(tr);
 }
