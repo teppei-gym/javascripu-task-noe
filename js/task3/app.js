16840337ddc371e2ad97a9282469a789a653949b
teppei <yamamutaisou@gmail.com>
2020-09-18 09:25:48 +0900

    関数の切り分け

--- js/task3/app.js
+++ js/task3/app.js
@@ -13,24 +13,12 @@ addBtn.addEventListener('click', function () {
 });
 
 function output(task, table) {
-  const tr = document.createElement('tr');
+  let tr = document.createElement('tr');
 
-  const appendTr = (tagName, value, classNanme = null) => {
-    const el = document.createElement(tagName);
-    if (classNanme) {
-      el.classList.add(classNanme);
-    }
-    if (value === '削除') {
-      el.addEventListener('click', deleteTask)
-    }
-    el.textContent = value;
-    tr.appendChild(el);
-  };
-
-  appendTr('td', ++maxTaskId, 'task-id');
-  appendTr('td', task);
-  appendTr('button', '作業中');
-  appendTr('button', '削除');
+  tr.appendChild(createTd('td', ++maxTaskId, 'task-id'));
+  tr.appendChild(createTd('td', task));
+  tr.appendChild(createTd('button', '作業中'));
+  tr.appendChild(createTd('button', '削除'));
 
   table.lastElementChild.appendChild(tr);
 }
@@ -38,9 +26,21 @@ function output(task, table) {
 function deleteTask(e) {
   e.target.parentNode.remove();
   const idElements = document.querySelectorAll('.task-id');
-  console.log(idElements)
   maxTaskId = 0;
   for (let el of [...idElements]) {
     el.textContent = ++maxTaskId;
   }
+}
+
+function createTd(tagName, value, classNanme = null) {
+  const el = document.createElement(tagName);
+  if (classNanme) {
+    el.classList.add(classNanme);
+  }
+  if (value === '削除') {
+    el.addEventListener('click', deleteTask)
+  }
+  el.textContent = value;
+
+  return el;
 }
