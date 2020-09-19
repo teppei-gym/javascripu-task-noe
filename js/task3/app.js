907a74648b8929b92f79d25046e331ca1f4a5c04
teppei <yamamutaisou@gmail.com>
2020-09-19 15:50:48 +0900

    課題３−３　完了

--- js/task3/app.js
+++ js/task3/app.js
@@ -32,14 +32,24 @@ function deleteTask(e) {
   }
 }
 
+function changeStatus(e) {
+  e.target.textContent = e.target.textContent === '作業中' ? '完了' : '作業中';
+}
+
 function createTd(tagName, value, classNanme = null) {
   const el = document.createElement(tagName);
   if (classNanme) {
     el.classList.add(classNanme);
   }
+
   if (value === '削除') {
     el.addEventListener('click', deleteTask)
   }
+
+  if (value === '作業中') {
+    el.addEventListener('click', changeStatus);
+  }
+
   el.textContent = value;
 
   return el;
